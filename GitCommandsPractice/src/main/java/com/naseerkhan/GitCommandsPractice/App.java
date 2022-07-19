cfa55ad4125daae9cdad5218d9a3d2c8a966a846
Naseer Khan <naseerkhan200@gmail.com>
2022-07-19 17:35:17 +0530

    App.java class committed

---
+++ GitCommandsPractice/src/main/java/com/naseerkhan/GitCommandsPractice/App.java
@@ -0,0 +1,14 @@
+package com.naseerkhan.GitCommandsPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("First testing");
+        System.out.println( "Hello World!" );
+    }
+}
